ee6a22f5460fa4e958f85cc77090f8bae89d66cb
Yurii_Lykhva <yurii_lykhva@epam.com>
2025-05-14 10:00:27 +0300

    Implemented integration with Reportportal

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -1,15 +1,19 @@
 package core;
 
+import com.epam.reportportal.junit5.ReportPortalExtension;
+import com.epam.reportportal.testng.ReportPortalTestNGListener;
 import core.utils.SlackService;
 import core.utils.TestListener;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 
-@Listeners({TestListener.class})
+@Listeners({TestListener.class, ReportPortalTestNGListener.class})
+@ExtendWith(ReportPortalExtension.class)
 public class BaseTest {
     public SlackService slackService = new SlackService();
 
